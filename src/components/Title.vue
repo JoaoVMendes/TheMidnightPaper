2a192c9df29c9e85f726f26f66561fcfd83e7f05
JoãoVMendes <jvmendes94@hotmail.com>
2022-11-23 19:05:00 -0300

    New info implementation on title component: Volume Number, date and Price.

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,9 +1,13 @@
 <template>
-    <div>
+    <div class="titleSection">
         <hr>
         <h1 class="title">The Midnight Paper</h1>
         <hr>
-        <h4 class="text">Friday, November 11th, 2023</h4>
+        <div class="volumeInfo">
+          <h4 class="text">VOL. 1</h4>
+          <h4 class="text">Friday, November 11th, 2023</h4>
+          <h4 class="text">Price: Free</h4>
+        </div>
         <hr style="margin-bottom: 25px;">
     </div>
 </template>
@@ -19,6 +23,16 @@ export default{
   font-family: "FaithCollapsing";
   src: local("FaithCollapsing"),
    url(@/fonts/Faith-Collapsing.ttf) format("truetype");
+}
+
+.titleSection{
+  width: 95%;
+  margin: auto;
+}
+
+.volumeInfo{
+  display: flex;
+  justify-content: space-between;
 }
 
 h1.title{
@@ -38,7 +52,7 @@ h4{
 }
 
 hr{
-  width: 95%;
+  width: 100%;
   border-color: rgb(49, 49, 49);
 }
 </style>
