43b991f851f799fc25ee5bac647f852c7f20f140
JoãoVMendes <jvmendes94@hotmail.com>
2022-12-04 01:31:52 -0300

    Removed unique Id from the scraping script. This caused the code to search for a specific post on a page that would not exist anymore after a week.

--- CreepySpider.js
+++ CreepySpider.js
@@ -9,7 +9,7 @@ async function crawl(){
     await page.goto('https://www.reddit.com/r/nosleep/top/?t=week')
 
     //Getting the first url of the top voted story of the week
-    var element = await page.$('#t3_z3ls5b > div._1poyrkZ7g36PawDueRza-J._11R7M_VOgKO1RJyRSRErT3 > div._2FCtq-QzlfuN-SwVMUZMM3._3wiKjmhpIpoTE2r5KCm2o6.t3_z3ls5b > div.y8HYJ-y_lTUHkQIc1mdCq._2INHSNB8V5eaWp4P0rY_mE > a')
+    var element = await page.$('div._1poyrkZ7g36PawDueRza-J._11R7M_VOgKO1RJyRSRErT3 > div._2FCtq-QzlfuN-SwVMUZMM3._3wiKjmhpIpoTE2r5KCm2o6 > div.y8HYJ-y_lTUHkQIc1mdCq._2INHSNB8V5eaWp4P0rY_mE > a')
     const url = await element.evaluate(el => el.href)
     
     //Open the top voted post page.
@@ -18,24 +18,24 @@ async function crawl(){
     })
 
     //Get the post Title
-    element = await page.$('#t3_z3ls5b > div > div._2FCtq-QzlfuN-SwVMUZMM3._2v9pwVh0VUYrmhoMv1tHPm.t3_z3ls5b > div.y8HYJ-y_lTUHkQIc1mdCq._2INHSNB8V5eaWp4P0rY_mE > div > h1')
+    element = await page.$('._2FCtq-QzlfuN-SwVMUZMM3._2v9pwVh0VUYrmhoMv1tHPm > div.y8HYJ-y_lTUHkQIc1mdCq._2INHSNB8V5eaWp4P0rY_mE > div > h1')
     const postTitle = await element.evaluate(el => el.innerText)
     //console.log(postTitle)
 
     //Wait for the selector to load because this one is dynamicaly inserted a few seconds later by javascript
     //Get the post Author.
-    element = await page.waitForSelector('#t3_z3ls5b > div > div._14-YvdFiW5iVvfe5wdgmET > div > div._3AStxql1mQsrZuUIFP9xSg.nU4Je7n-eSXStTBAPMYt8 > div')
+    element = await page.waitForSelector('._14-YvdFiW5iVvfe5wdgmET > div > div._3AStxql1mQsrZuUIFP9xSg.nU4Je7n-eSXStTBAPMYt8 > div')
     const postAuthor = await element.evaluate(el => el.innerText)
     //console.log(postAuthor.substring(2, postAuthor.length))
 
     //Get the post Upvotes
-    element = await page.$('#vote-arrows-t3_z3ls5b > div')
+    element = await page.$('._1rZYMD_4xY3gRcSS3p8ODO._3a2ZHWaih05DgAOtvu6cIo')
     const postUpvotes = await element.evaluate(el => el.innerText)
     //console.log(postUpvotes)
 
     //Get the text by paragraphs and save it into an array
     const postText = await page.evaluate(() =>{
-        const paragraphs = Array.from(document.querySelectorAll('#t3_z3ls5b > div > div._3xX726aBn29LDbsDtzr_6E._1Ap4F5maDtT1E1YuCiaO0r.D3IL3FD0RFy_mkKLPwL4 > div > p'), x => x.innerText)
+        const paragraphs = Array.from(document.querySelectorAll('._3xX726aBn29LDbsDtzr_6E._1Ap4F5maDtT1E1YuCiaO0r.D3IL3FD0RFy_mkKLPwL4 > div > p'), x => x.innerText)
         return paragraphs
     })
     //console.log(postText)
